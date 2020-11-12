d4001c42bd9387915e8f82150d5d14d093f7a04d
Esteban <Esteban@DESKTOP-SobreMe>
2020-11-12 17:28:23 +0100

    [estsvq](Explained detailed in body) Some files edited and jsp created.

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,14 +1,19 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class HelloWorldController {
-	@RequestMapping("/")  
-	public String hello()   
-	{  
-	return "Hello User";  
-	}  
 
+    @GetMapping({"/","/index","/inicio"})
+    public String hello(@RequestParam(value = "name", defaultValue = "Carlos, Adri, Esteban, Adil, Juan y José",
+        required = true) String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
 }
+
