c76d3f88c2d3aadca512a46dc58dfe0edf0cb8bb
estsvq <ezasdi@gmail.com>
2020-11-15 20:29:30 +0100

    [Esteban] Added some example login page.

--- src/main/java/com/project/TabernasSevilla/model/IndexController.java
+++ src/main/java/com/project/TabernasSevilla/model/IndexController.java
@@ -2,16 +2,29 @@ package com.project.TabernasSevilla.model;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 
-	@GetMapping({"/","/index","/inicio"})
+	@RequestMapping({"/","/index","/inicio"})
     public String hello(@RequestParam(value = "name", defaultValue = "Carlos, Adrian H., Esteban, Adil, Juan y José",
         required = true) String name, Model model) {
         model.addAttribute("name", name);
         return "index";
     }
+	
+	@RequestMapping({"/login"})
+    public String login(Model model) {
+        return "login";
+    }
+	
+	@RequestMapping({"/register"})
+    public String register(@RequestParam(value = "name", defaultValue = "persona registrada. ¿Cómo estás?",
+    	required = true) String name, Model model) {
+		model.addAttribute("name", name);
+        return "index";
+    }
 }
+
