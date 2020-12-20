0feb2a1ea3054b988e4ee501438d546a10d7e150
estsvq <ezasdi@gmail.com>
2020-12-20 23:32:22 +0100

    [esteban] Implemented server and clients validators part 1

--- src/main/java/com/project/TabernasSevilla/forms/ContactForm.java
+++ src/main/java/com/project/TabernasSevilla/forms/ContactForm.java
@@ -1,12 +1,28 @@
 package com.project.TabernasSevilla.forms;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
-public class ContactForm { //no hay validaciones porque se pueden poner en el html 
+public class ContactForm { 
 	
+	//est - validaciones hechas.
+	
+	@NotEmpty(message = "You must put your name and surname to contact you, thank you.")
+	@Size(min = 2, max = 128)
+	//To allow "tildes"
+	@Pattern(regexp ="[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]" 
+	, message = "Please enter your first and last name correctly.")
 	private String fullName;
+	
+	@NotEmpty(message = "The field must be correctly filled in") 
+	@Email(message = "Please enter a valid email address.")
 	private String email;
+	
 	private String cv;
 
 }
