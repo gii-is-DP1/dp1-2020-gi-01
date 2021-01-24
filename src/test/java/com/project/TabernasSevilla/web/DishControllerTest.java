efbf2bd360ac885574636def6af65ac5db84aa9f
carnuare <carnuare@gmail.com>
2021-01-24 14:39:05 +0100

    working dish controller test

--- src/test/java/com/project/TabernasSevilla/web/DishControllerTest.java
+++ src/test/java/com/project/TabernasSevilla/web/DishControllerTest.java
@@ -1,22 +1,15 @@
 package com.project.TabernasSevilla.web;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.stereotype.Service;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.project.TabernasSevilla.configuration.SecurityConfiguration;
@@ -24,15 +17,21 @@ import com.project.TabernasSevilla.controller.DishController;
 import com.project.TabernasSevilla.domain.Dish;
 import com.project.TabernasSevilla.domain.Establishment;
 import com.project.TabernasSevilla.domain.Seccion;
-import com.project.TabernasSevilla.repository.EstablishmentRepository;
+import com.project.TabernasSevilla.repository.*;
+import com.project.TabernasSevilla.security.AuthorityRepository;
 import com.project.TabernasSevilla.security.UserService;
 import com.project.TabernasSevilla.service.ActorService;
 import com.project.TabernasSevilla.service.DishService;
 import com.project.TabernasSevilla.service.EstablishmentService;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //@RunWith(SpringRunner.class)
-@WebAppConfiguration
-@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+//@WebAppConfiguration
 @WebMvcTest(controllers = DishController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class, includeFilters = {
 		@ComponentScan.Filter(Service.class) })
 //@MockBean(JpaMetamodelMappingContext.class) //para que evite buscar la database
@@ -41,22 +40,62 @@ public class DishControllerTest {
 	@Autowired
 	private DishController dishController;
 
-	@Autowired
+	@MockBean
 	private UserService userService;
 
-	@Autowired
+	@MockBean
 	private ActorService actorService;
 
-	@Autowired
+	@MockBean
 	private DishService dishService;
 
-	@Autowired
+	@MockBean
 	private EstablishmentService establishmentService;
-	
-	@Autowired
+
+	@MockBean
+	private ReviewRepository reviewRepository;
+
+	@MockBean
+	private AuthorityRepository authRepository;
+
+	@MockBean
+	private AdminRepository adminRepository;
+
+	@MockBean
+	private BookingRepository bookingRepository;
+
+	@MockBean
 	private EstablishmentRepository establishmentRepository;
 
-	
+	@MockBean
+	private TableRepository tableRepository;
+
+	@MockBean
+	private CurriculumRepository cur;
+
+	@MockBean
+	private CookRepository cook;
+
+	@MockBean
+	private CustomerRepository cus;
+
+	@MockBean
+	private OrderRepository ord;
+
+	@MockBean
+	private RegKeyRepository reg;
+
+	@MockBean
+	private WaiterRepository wait;
+
+	@MockBean
+	private ManagerRepository man;
+
+	@MockBean
+	private OrderCancellationRepository canc;
+
+	@MockBean
+	private OrderLogRepository logga;
 
 	// POR ALGUN MOTIVO HE TENIDO QUE CREAR TODOS ESTOS MOCKBEANS PARA QUE FUNCIONE
 	// EL TEST SIMPLE DE HTTPRESPONSE
